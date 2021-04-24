6bf236d33aae38c7e90dd31eb3879b9b2264616b
Simay Avcı <67237751+simayavci@users.noreply.github.com>
2021-04-25 00:22:12 +0300

    Update RFM.sql

--- RFM.sql
+++ RFM.sql
@@ -1,21 +1,17 @@
-
-
-with rfm as
-(
+--SQL ile RFM Analizi
 Select o.CustomerID,
 datediff(DAY,MAX(o.OrderDate), GETDATE()) as recency,
-count(distinct o.OrderDate) as frequency,
+count(o.CustomerID) as frequency,
 SUM((od.Quantity*od.UnitPrice)) AS monetary 
+into #rfm --Bulunan değerlerin temp table'a kaydedilmesi
 from Orders o inner join [Order Details] od on od.OrderID=o.OrderID
 Group by o.CustomerID
-)
 
-Select rfm.CustomerID,
-      ntile(5) over (order by rfm.recency) as r,
-      ntile(5) over (order by rfm.frequency) as f,
-      ntile(5) over (order by rfm.monetary) as m
+Select #rfm.CustomerID,
+      ntile(5) over (order by #rfm.recency) as r,--recency değerlerinin 5'e bölerek kümelendirilmesi
+      ntile(5) over (order by #rfm.frequency) as f,--frequency değerlerinin 5'e bölerek kümelendirilmesi
+      ntile(5) over (order by #rfm.monetary) as m --monetary değerlerinin 5'e bölerek kümelendirilmesi
 	 
-from rfm
-Group by rfm.CustomerID,rfm.recency,rfm.frequency,rfm.monetary
-Order by rfm.monetary
-
+from #rfm
+Group by #rfm.CustomerID,#rfm.recency,#rfm.frequency,#rfm.monetary
+Order by #rfm.monetary
